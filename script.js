08426c40a846115b6a4d5bef411efd192872a942
Mohamed <moamrsabry2005@gmail.com>
2025-03-09 02:46:13 +0200

    unstyled swiper

--- script.js
+++ script.js
@@ -125,3 +125,28 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+var swiper = new Swiper(".vehicles-slider", {
+    grabCursor: true,
+    centeredSlides: true,  
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
