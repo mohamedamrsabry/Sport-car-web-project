d86967b03977511f7f4ff9bc6fbb1af26a0c98d8
chris <chrisnashef61@gmail.com>
2025-03-11 14:10:14 +0200

    about us

--- script.js
+++ script.js
@@ -116,4 +116,17 @@ var swiper = new Swiper(".vehicles-slider", {
       prevEl: ".swiper-button-prev",
     },
   });
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("nav ul li a").forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop - 50,
+                behavior: "smooth"
+            });
+        });
+    });
+});
   
