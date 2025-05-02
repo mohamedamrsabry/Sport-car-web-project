af37bfa19c8ad1922d221cfaf50751f2a0bcdc02
Marwan <muhammedmarwan120@gmail.com>
2025-05-02 04:00:35 +0300

    done

--- products.js
+++ products.js
@@ -34,306 +34,305 @@ sidebarToggle.addEventListener('click', () => {
 });
 
 const cars = [
-    {
-      make: "Mercedes-Benz",
-      model: "AMG ONE",
-      year: 2025,
-      price: "80,000,000 EGP",
-      mileage: 12,
-      color: "Red",
-      condition: "New",
-      image: "mercedes_amg_one.jpg"
-    },
-    {
-      make: "Porsche",
-      model: "Cayenne GTS",
-      year: 2025,
-      price: "5,000,000 EGP",
-      mileage: 7,
-      color: "White",
-      condition: "New",
-      image: "porsche_cayenne_gts.jpg"
-    },
-    {
-      make: "BMW",
-      model: "M4 Competition (G82)",
-      year: 2025,
-      price: "4,200,000 EGP",
-      mileage: 18,
-      color: "Blue",
-      condition: "New",
-      image: "bmw_m4_g82.jpg"
-    },
-    {
-      make: "Ferrari",
-      model: "SF90 Stradale",
-      year: 2025,
-      price: "19,000,000 EGP",
-      mileage: 5,
-      color: "Yellow",
-      condition: "New",
-      image: "ferrari_sf90.jpg"
-    },
-    {
-      make: "Audi",
-      model: "RS6 Avant Performance",
-      year: 2025,
-      price: "4,000,000 EGP",
-      mileage: 22,
-      color: "Grey",
-      condition: "New",
-      image: "audi_rs6.jpg"
-    },
-    {
-      make: "McLaren",
-      model: "Artura",
-      year: 2025,
-      price: "7,000,000 EGP",
-      mileage: 16,
-      color: "Orange",
-      condition: "New",
-      image: "mclaren_artura.jpg"
-    },
-    {
-      make: "Rolls-Royce",
-      model: "Phantom",
-      year: 2025,
-      price: "17,000,000 EGP",
-      mileage: 8,
-      color: "Black",
-      condition: "New",
-      image: "rolls_royce_phantom.jpg"
-    },
-    {
-      make: "Lamborghini",
-      model: "Urus Performante",
-      year: 2025,
-      price: "8,000,000 EGP",
-      mileage: 20,
-      color: "Green",
-      condition: "New",
-      image: "lamborghini_urus_performante.jpg"
-    },
-    {
-      make: "Porsche",
-      model: "Panamera Turbo S E-Hybrid",
-      year: 2025,
-      price: "10,000,000 EGP",
-      mileage: 11,
-      color: "Silver",
-      condition: "New",
-      image: "porsche_panamera_turbo_s_ehybrid.jpg"
-    },
-    {
-      make: "Bentley",
-      model: "Continental GT Speed",
-      year: 2025,
-      price: "8,000,000 EGP",
-      mileage: 14,
-      color: "Blue",
-      condition: "New",
-      image: "bentley_continental_gt_speed.jpg"
-    },
-    {
-      make: "BMW",
-      model: "M5 Competition (F90)",
-      year: 2025,
-      price: "5,500,000 EGP",
-      mileage: 9,
-      color: "Black",
-      condition: "New",
-      image: "bmw_m5_f90.jpg"
-    },
-    {
-      make: "Mercedes-Benz",
-      model: "G 63",
-      year: 2025,
-      price: "7,000,000 EGP",
-      mileage: 13,
-      color: "White",
-      condition: "New",
-      image: "mercedes_g63.jpg"
-    },
-    {
-      make: "Audi",
-      model: "R8 V10 Performance Quattro",
-      year: 2025,
-      price: "6,000,000 EGP",
-      mileage: 17,
-      color: "Red",
-      condition: "New",
-      image: "audi_r8_v10.jpg"
-    },
-    {
-      make: "Rolls-Royce",
-      model: "Cullinan",
-      year: 2025,
-      price: "10,000,000 EGP",
-      mileage: 6,
-      color: "Silver",
-      condition: "New",
-      image: "rolls_royce_cullinan.jpg"
-    },
-    {
-      make: "McLaren",
-      model: "P1",
-      year: 2025,
-      price: "35,000,000 EGP",
-      mileage: 5,
-      color: "Orange",
-      condition: "New",
-      image: "mclaren_p1.jpg"
-    },
-    {
-      make: "Porsche",
-      model: "911 Turbo S",
-      year: 2025,
-      price: "8,000,000 EGP",
-      mileage: 10,
-      color: "Black",
-      condition: "New",
-      image: "porsche_911_turbo_s.jpg"
-    },
-    {
-      make: "Bentley",
-      model: "Flying Spur Speed",
-      year: 2025,
-      price: "7,000,000 EGP",
-      mileage: 12,
-      color: "Grey",
-      condition: "New",
-      image: "bentley_flying_spur_speed.jpg"
-    },
-    {
-      make: "BMW",
-      model: "M3 Competition (G80)",
-      year: 2025,
-      price: "4,000,000 EGP",
-      mileage: 15,
-      color: "White",
-      condition: "New",
-      image: "bmw_m3_g80.jpg"
-    },
-    {
-      make: "Mercedes-Benz",
-      model: "SL 63 S E Performance",
-      year: 2025,
-      price: "10,000,000 EGP",
-      mileage: 9,
-      color: "Blue",
-      condition: "New",
-      image: "mercedes_sl63_s_e.jpg"
-    },
-    {
-      make: "Ferrari",
-      model: "812 Superfast",
-      year: 2025,
-      price: "10,000,000 EGP",
-      mileage: 7,
-      color: "Red",
-      condition: "New",
-      image: "ferrari_812_superfast.jpg"
-    },
-    {
-      make: "Lamborghini",
-      model: "Revuelto",
-      year: 2025,
-      price: "13,000,000 EGP",
-      mileage: 14,
-      color: "Black",
-      condition: "New",
-      image: "lamborghini_revuelto.jpg"
-    },
-    {
-      make: "Audi",
-      model: "RS7 Sportback Performance",
-      year: 2025,
-      price: "4,000,000 EGP",
-      mileage: 19,
-      color: "Grey",
-      condition: "New",
-      image: "audi_rs7.jpg"
-    },
-    {
-      make: "BMW",
-      model: "M4 Competition (F82)",
-      year: 2025,
-      price: "3,500,000 EGP",
-      mileage: 20,
-      color: "Blue",
-      condition: "New",
-      image: "bmw_m4_f82.jpg"
-    },
-    {
-      make: "McLaren",
-      model: "750S",
-      year: 2025,
-      price: "9,000,000 EGP",
-      mileage: 11,
-      color: "White",
-      condition: "New",
-      image: "mclaren_750s.jpg"
-    },
-    {
-      make: "Bentley",
-      model: "Bentayga Speed",
-      year: 2025,
-      price: "7,000,000 EGP",
-      mileage: 13,
-      color: "Green",
-      condition: "New",
-      image: "bentley_bentayga_speed.jpg"
-    },
-    {
-      make: "Ferrari",
-      model: "Purosangue",
-      year: 2025,
-      price: "20,000,000 EGP",
-      mileage: 8,
-      color: "Yellow",
-      condition: "New",
-      image: "ferrari_purosangue.jpg"
-    },
-    {
-      make: "Mercedes-Benz",
-      model: "S 680 4MATIC",
-      year: 2025,
-      price: "9,000,000 EGP",
-      mileage: 10,
-      color: "Black",
-      condition: "New",
-      image: "maybach_s680.jpg"
-    },
-    {
-      make: "Audi",
-      model: "RS Q8",
-      year: 2025,
-      price: "5,000,000 EGP",
-      mileage: 17,
-      color: "White",
-      condition: "New",
-      image: "audi_rs_q8.jpg"
-    },
-    {
-      make: "Mercedes-Benz",
-      model: "AMG GT 63 S E Performance",
-      year: 2025,
-      price: "12,000,000 EGP",
-      mileage: 14,
-      color: "Silver",
-      condition: "New",
-      image: "mercedes_gt63_s_e.jpg"
-    },
-    {
-      make: "Rolls-Royce",
-      model: "Ghost",
-      year: 2025,
-      price: "10,000,000 EGP",
-      mileage: 6,
-      color: "Grey",
-      condition: "New",
-      image: "rolls_royce_ghost.jpg"
-    }
+  {
+    make: "Mercedes-Benz",
+    model: "AMG ONE",
+    year: 2025,
+    price: "80,000,000 EGP",
+    mileage: 12,
+    color: "Red",
+    condition: "New",
+    image: "img/products/mercedes benz amg one.jpg"
+  },
+  {
+    make: "Porsche",
+    model: "Cayenne GTS",
+    year: 2025,
+    price: "5,000,000 EGP",
+    mileage: 7,
+    color: "White",
+    condition: "New",
+    image: "img/products/porche cayenne gts.jpg"
+  },
+  {
+    make: "BMW",
+    model: "M4 Competition (G82)",
+    year: 2025,
+    price: "4,200,000 EGP",
+    mileage: 18,
+    color: "Blue",
+    condition: "New",
+    image: "img/products/BMW M4 Competition (G82).jpg"
+  },
+  {
+    make: "Ferrari",
+    model: "SF90 Stradale",
+    year: 2025,
+    price: "19,000,000 EGP",
+    mileage: 5,
+    color: "Yellow",
+    condition: "New",
+    image: "img/products/Ferrari SF90 Stradale.jpg"
+  },
+  {
+    make: "Audi",
+    model: "RS6 Avant Performance",
+    year: 2025,
+    price: "4,000,000 EGP",
+    mileage: 22,
+    color: "Grey",
+    condition: "New",
+    image: "img/products/Audi RS6 Avant Performance.jpg"
+  },
+  {
+    make: "McLaren",
+    model: "Artura",
+    year: 2025,
+    price: "7,000,000 EGP",
+    mileage: 16,
+    color: "Orange",
+    condition: "New",
+    image: "img/products/McLaren Artura.jpg"
+  },
+  {
+    make: "Rolls-Royce",
+    model: "Phantom",
+    year: 2025,
+    price: "17,000,000 EGP",
+    mileage: 8,
+    color: "Black",
+    condition: "New",
+    image: "img/products/Rolls-Royce Phantom.jpg"
+  },
+  {
+    make: "Lamborghini",
+    model: "Urus Performante",
+    year: 2025,
+    price: "8,000,000 EGP",
+    mileage: 20,
+    color: "Green",
+    condition: "New",
+    image: "img/products/Lamborghini Urus Performante.jpg"
+  },
+  {
+    make: "Porsche",
+    model: "Panamera Turbo S E-Hybrid",
+    year: 2025,
+    price: "10,000,000 EGP",
+    mileage: 11,
+    color: "Silver",
+    condition: "New",
+    image: "img/products/Porsche Panamera Turbo S E-Hybrid.jpg"
+  },
+  {
+    make: "Bentley",
+    model: "Continental GT Speed",
+    year: 2025,
+    price: "8,000,000 EGP",
+    mileage: 14,
+    color: "Blue",
+    condition: "New",
+    image: "img/products/Bentley Continental GT Speed.jpg"
+  }, {
+    make: "BMW",
+    model: "M5 Competition (F90)",
+    year: 2025,
+    price: "5,500,000 EGP",
+    mileage: 9,
+    color: "Black",
+    condition: "New",
+    image: "img/products/BMW M5 Competition (F90).jpg"
+  },
+  {
+    make: "Mercedes-Benz",
+    model: "G 63",
+    year: 2025,
+    price: "7,000,000 EGP",
+    mileage: 13,
+    color: "White",
+    condition: "New",
+    image: "img/products/Mercedes-Benz G 63.jpg"
+  },
+  {
+    make: "Audi",
+    model: "R8 V10 Performance Quattro",
+    year: 2025,
+    price: "6,000,000 EGP",
+    mileage: 17,
+    color: "Red",
+    condition: "New",
+    image: "img/products/Audi R8 V10 Performance Quattro.jpg"
+  },
+  {
+    make: "Rolls-Royce",
+    model: "Cullinan",
+    year: 2025,
+    price: "10,000,000 EGP",
+    mileage: 6,
+    color: "Silver",
+    condition: "New",
+    image: "img/products/Rolls-Royce Cullinan.jpg"
+  },
+  {
+    make: "McLaren",
+    model: "P1",
+    year: 2025,
+    price: "35,000,000 EGP",
+    mileage: 5,
+    color: "Orange",
+    condition: "New",
+    image: "img/products/McLaren P1.jpg"
+  },
+  {
+    make: "Porsche",
+    model: "911 Turbo S",
+    year: 2025,
+    price: "8,000,000 EGP",
+    mileage: 10,
+    color: "Black",
+    condition: "New",
+    image: "img/products/Porsche 911 Turbo S.jpg"
+  },
+  {
+    make: "Bentley",
+    model: "Flying Spur Speed",
+    year: 2025,
+    price: "7,000,000 EGP",
+    mileage: 12,
+    color: "Grey",
+    condition: "New",
+    image: "img/products/Bentley Flying Spur Speed.jpg"
+  },
+  {
+    make: "BMW",
+    model: "M3 Competition (G80)",
+    year: 2025,
+    price: "4,000,000 EGP",
+    mileage: 15,
+    color: "White",
+    condition: "New",
+    image: "img/products/BMW M3 Competition (G80).jpg"
+  },
+  {
+    make: "Mercedes-Benz",
+    model: "SL 63 S E Performance",
+    year: 2025,
+    price: "10,000,000 EGP",
+    mileage: 9,
+    color: "Blue",
+    condition: "New",
+    image: "img/products/Mercedes-Benz SL 63 S E Performance.jpg"
+  },
+  {
+    make: "Ferrari",
+    model: "812 Superfast",
+    year: 2025,
+    price: "10,000,000 EGP",
+    mileage: 7,
+    color: "Red",
+    condition: "New",
+    image: "img/products/Ferrari 812 Superfast.jpg"
+  },
+  {
+    make: "Lamborghini",
+    model: "Revuelto",
+    year: 2025,
+    price: "13,000,000 EGP",
+    mileage: 14,
+    color: "Black",
+    condition: "New",
+    image: "img/products/Lamborghini Revuelto.jpg"
+  },
+  {
+    make: "Audi",
+    model: "RS7 Sportback Performance",
+    year: 2025,
+    price: "4,000,000 EGP",
+    mileage: 19,
+    color: "Grey",
+    condition: "New",
+    image: "img/products/Audi RS7 Sportback Performance.jpg"
+  },
+  {
+    make: "BMW",
+    model: "M4 Competition (F82)",
+    year: 2025,
+    price: "3,500,000 EGP",
+    mileage: 20,
+    color: "Blue",
+    condition: "New",
+    image: "img/products/BMW M4 Competition (F82).jpg"
+  },
+  {
+    make: "McLaren",
+    model: "750S",
+    year: 2025,
+    price: "9,000,000 EGP",
+    mileage: 11,
+    color: "White",
+    condition: "New",
+    image: "img/products/McLaren 750S.jpg"
+  },
+  {
+    make: "Bentley",
+    model: "Bentayga Speed",
+    year: 2025,
+    price: "7,000,000 EGP",
+    mileage: 13,
+    color: "Green",
+    condition: "New",
+    image: "img/products/Bentley Bentayga Speed.jpg"
+  },
+  {
+    make: "Ferrari",
+    model: "Purosangue",
+    year: 2025,
+    price: "20,000,000 EGP",
+    mileage: 8,
+    color: "Yellow",
+    condition: "New",
+    image: "img/products/Ferrari Purosangue.jpg"
+  },
+  {
+    make: "Mercedes-Benz",
+    model: "S 680 4MATIC",
+    year: 2025,
+    price: "9,000,000 EGP",
+    mileage: 10,
+    color: "Black",
+    condition: "New",
+    image: "img/products/Mercedes-Benz S 680 4MATIC.jpg"
+  },
+  {
+    make: "Audi",
+    model: "RS Q8",
+    year: 2025,
+    price: "5,000,000 EGP",
+    mileage: 17,
+    color: "White",
+    condition: "New",
+    image: "img/products/Audi RS Q8.jpg"
+  },
+  {
+    make: "Mercedes-Benz",
+    model: "AMG GT 63 S E Performance",
+    year: 2025,
+    price: "12,000,000 EGP",
+    mileage: 14,
+    color: "Silver",
+    condition: "New",
+    image: "img/products/Mercedes-Benz AMG GT 63 S E Performance.jpg"
+  },
+  {
+    make: "Rolls-Royce",
+    model: "Ghost",
+    year: 2025,
+    price: "10,000,000 EGP",
+    mileage: 6,
+    color: "Grey",
+    condition: "New",
+    image: "img/products/Rolls-Royce Ghost.jpg"
+  }
   ];
   
   
