68e8f0d6a6d8a78b06743e0b4cda9d440e56d12b
mohamed <moamrsabry2005@gmail.com>
2025-06-10 19:08:51 +0300

    emaill

---
+++ backend/php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'strada-auto/email-service',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a268d41ed3e8b8a723c65b3c64f0cd7eb5d9a656',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'strada-auto/email-service' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a268d41ed3e8b8a723c65b3c64f0cd7eb5d9a656',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
