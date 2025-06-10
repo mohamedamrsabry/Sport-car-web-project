4bbe496fceeb9918ec4611d1b145d58ae46c7744
Marwan <muhammedmarwan120@gmail.com>
2025-06-10 19:46:02 +0300

    email subscribe

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,22 @@
+
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const subscribeRoute = require('./routes/subscribeRoute');
+
+// Add this with your other middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Add this with your other routes
+app.use('/api', subscribeRoute);
+
+// Rest of your existing db.js code...
+
+
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
@@ -11,3 +30,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
